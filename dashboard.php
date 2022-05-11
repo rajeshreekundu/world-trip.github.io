fc92823f21a966a99eda8e6b2e4faba2761a321d
rajeshreekundu <rajeshreekundu14@gmail.com>
2022-05-11 22:17:53 +0530

    Test

---
+++ dashboard.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class=' d-flex'>
+        <div id="mySidebar" class="dshbrd-sidebar flex-fill">
+            <!-- <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a> -->
+            <span class="closebtn" onclick="closeNav()">×</span>
+            <div class='dshbrd-logo'>
+                <img src="../../images/logo-light2.png" alt="">
+            </div>
+            <!-- <form action="seller_dashboard.php" method="get"> -->
+
+            <ul class='inner-sidebar'>
+                <li id='id1' class='active-menu'>
+                    <i class="tgs-icon-dashboard"></i>
+                    <span>Dashboard</span>
+                </li>
+                <li id='id2'>
+                    <i class="tgs-icon-box-bookmark"></i>
+                    <span>Product</span>
+                </li>
+                <li id='id3'>
+                    <i class="tgs-icon-document-list"></i>
+                    <span>Order</span>
+                </li>
+                <li id='id4'>
+                <i class="tgs-icon-group"></i>
+                    <span>Customers</span>
+                </li>
+                <li id='id5'>
+                    <i class="tgs-icon-credit-card"></i>
+                    <span>Payments</span>
+                </li>
+                <li id='id6'>
+                    <i class="tgs-icon-chat"></i>
+                    <span>Feedback</span>
+                </li>
+                <li id='id7'>
+                    <i class="tgs-icon-box-add"></i>
+                    <span>Inbox</span>
+                </li>
+                <li id='id8' class='dshbrd-logout'>
+                    <i class="tgs-icon-power_settings_new"></i>
+                    <span>Logout</span>
+                </li>
+
+            </ul>
+        </div>
+
+        <!-- </form> -->
+
+        <div id="main" class='dshbrd-main flex-fill'>
+            <h1>Hello World</h1>
+        </div>
+    </div>
+</body>
+</html>
